3ffab346917dcce7dfca73c2fd4dc15a470fc31b
Janne Lammela <lammelajanne1@gmail.com>
2023-11-22 22:06:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+bool end = false;
+
+while (!end)
+{
+    string command = string.Empty;
+    bool success = false;
+    int[] numbers = [];
+
+    while (!success)
+    {
+        command = Console.ReadLine();
+        command = string.Join("-", command.ToCharArray().Where(Char.IsDigit));
+        if (!string.IsNullOrWhiteSpace(command))
+        {
+            numbers = command.Split('-').Select(int.Parse).ToArray();
+            if (numbers.Length > 11)
+            {
+                Console.WriteLine("You charge too much!");
+                continue;
+            }
+            else
+            {
+                success = true;
+            }
+            
+        } 
+        else
+        {
+            Console.WriteLine("Something went wrong with input!");
+            continue;
+        }
+    }
+
+    Console.WriteLine(numbers.Length);
+}
